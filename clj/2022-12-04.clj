38b80eb50b66251daf2ae454585af8c6dfbae280
brennop <brennopcordeiro@gmail.com>
2022-12-04 19:16:49 -0300

    day 4 improved clj

--- clj/2022-12-04.clj
+++ clj/2022-12-04.clj
@@ -1,11 +1,11 @@
 (defn get-values [line]
-  (vec (map #(Integer/parseInt %) (rest (re-find #"(\d+)-(\d+),(\d+)-(\d+)" line)))))
+  (->> line
+    (re-find #"(\d+)-(\d+),(\d+)-(\d+)")
+    (rest)
+    (map #(Integer/parseInt %))))
 
 (defn reducer [sum line]
   (let [[a b c d] (get-values line)]
-          (cond
-            (and (>= c a) (<= c b)) (inc sum)
-            (and (>= a c) (<= a d)) (inc sum)
-            :else sum)))
+    (+ sum (if (and (<= a d) (<= c b)) 1 0))))
 
 (reduce reducer 0 *input*)
