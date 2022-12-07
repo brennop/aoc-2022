461fa8913a48452e3d1b5535dbfd408fddbcb336
brennop <brennopcordeiro@gmail.com>
2022-12-07 17:44:08 -0300

    2022-12-07

--- clj/2022-12-07.clj
+++ clj/2022-12-07.clj
@@ -2,13 +2,12 @@
 
 (defn reducer [[position tree] line]
   (let [[a b c] (str/split line #" ")]
-    (println position)
-    (println tree)
     (cond 
       (= line "$ ls") [position tree]
       (= line "$ cd ..") [(pop position) tree]
       (= (subs line 0 4) "$ cd") [(conj position c) tree]
-      (= (subs line 0 2) "dir") [position (assoc-in tree (conj position b) {})]
-      :else [position (assoc-in tree (conj position b) a)])))
+      (= (subs line 0 3) "dir") [position (assoc-in tree (conj position b) {})]
+      :else [position (assoc-in tree (conj position b) (Integer/parseInt a))])))
 
-(reduce reducer [[] {:/ {}}] *input*)
+(def tree (reduce reducer [[] {:/ {}}] *input*))
+
